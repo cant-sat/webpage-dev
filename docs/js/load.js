2582da267b353c5dab697c5c5a4428a4346ecf80
Varga Miklós Máté <varga.miklos.mate@MAIL.LOVASSY.HU>
2023-12-15 10:35:13 +0100

    might work now

--- docs/js/load.js
+++ docs/js/load.js
@@ -1,7 +1,7 @@
-if (location.protocol === 'http:' && window.location.pathname != "live" && window.location.pathname != "live.html") {
+if (location.protocol === 'http:' && window.location.pathname != "/live" && window.location.pathname != "/live.html") {
     window.location.href = "https://"+ window.location.hostname +"/" + window.location.pathname;
 }
-else if (location.protocol === 'https:' && window.location.pathname == "live" && window.location.pathname == "live.html") {
+else if (location.protocol === 'https:' && window.location.pathname == "/live" && window.location.pathname == "/live.html") {
     window.location.href = "http://"+ window.location.hostname +"/" + window.location.pathname;
 }
 
