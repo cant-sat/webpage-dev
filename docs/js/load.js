edeedb7f752b1ac333a98489176253de293ffa7b
Varga Miklós Máté <varga.miklos.mate@MAIL.LOVASSY.HU>
2023-12-15 10:28:23 +0100

    infó infó

--- docs/js/load.js
+++ docs/js/load.js
@@ -1,3 +1,11 @@
+if (location.protocol === 'http:' && window.location.pathname != "live" && window.location.pathname != "live.html") {
+    window.location.href = "https://"+ window.location.hostname +"/" + window.location.pathname;
+}
+else if (location.protocol === 'https:' && window.location.pathname == "live" && window.location.pathname == "live.html") {
+    window.location.href = "http://"+ window.location.hostname +"/" + window.location.pathname;
+}
+
+
 fetch('template.html')
     .then(response => response.text())
     .then(data => {
